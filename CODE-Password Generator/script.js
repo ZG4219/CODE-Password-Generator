2ca38c40f0d15865228a9a0504bcd48fab35caec
Dionne Angeles <jr8dionne@yahoo.com>
2022-10-04 14:23:45 -0700

    added numeric and lower case to be included in pasword

--- CODE-Password Generator/script.js
+++ CODE-Password Generator/script.js
@@ -25,3 +25,11 @@ var specialCharacters = [
   '.',
 ];
 
+
+// Array of numeric characters to be included in password
+var numericCharacters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+
+// Array of lowercase characters to be included in password
+var lowerCasedCharacters = [
+  'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l','m', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y',
+  'z'];
