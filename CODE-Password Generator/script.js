db8d5c11597942a8d103f29d9c2adf4647be7c93
Dionne Angeles <jr8dionne@yahoo.com>
2022-10-04 14:41:53 -0700

    added addEventListener

--- CODE-Password Generator/script.js
+++ CODE-Password Generator/script.js
@@ -201,3 +201,17 @@ for (var i = 0; i < guaranteedCharacters.length; i++) {
 // Transform the result into a string and pass into writePassword
 return result.join('');
 }
+
+// Get references to the #generate element
+var generateBtn = document.querySelector('#generate');
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector('#password');
+
+  passwordText.value = password;
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener('click', writePassword);
