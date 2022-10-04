49828f4e6fa9e7e698390531431b415f81ccef63
Dionne Angeles <jr8dionne@yahoo.com>
2022-10-04 14:38:02 -0700

    Push new random special character

--- CODE-Password Generator/script.js
+++ CODE-Password Generator/script.js
@@ -67,29 +67,102 @@ var upperCasedCharacters = [
 
 // Function to prompt user for password options
 function getPasswordOptions() {
-  // Variable to store length of password from user input
-  var length = parseInt(
+// Variable to store length of password from user input
+var length = parseInt(
     prompt('How many characters would you like your password to contain?'),
     10
   );
 
-  // Conditional statement to check if password length is a number. Prompts end if this evaluates false
+// Conditional statement to check if password length is a number. Prompts end if this evaluates false
   if (Number.isNaN(length)) {
     alert('Password length must be provided as a number');
     return null;
   }
   
   
-  // Conditional statement to check if password length is at least 8 characters long. Prompts end if this evaluates false
+// Conditional statement to check if password length is at least 8 characters long. Prompts end if this evaluates false
   if (length < 8) {
     alert('Password length must be at least 8 characters');
     return null;
   }
 
-  // Conditional statement to check if password length is less than 128 characters long. Prompts end if this evaluates false
+// Conditional statement to check if password length is less than 128 characters long. Prompts end if this evaluates false
   if (length > 128) {
     alert('Password length must less than 129 characters');
     return null;
   }
 
   
+// Variable to store boolean regarding the inclusion of special characters
+  var hasSpecialCharacters = confirm(
+    'Click OK to confirm including special characters.'
+  );
+
+// Variable to store boolean regarding the inclusion of numeric characters
+  var hasNumericCharacters = confirm(
+    'Click OK to confirm including numeric characters.'
+  );
+
+// Variable to store boolean regarding the inclusion of lowercase characters
+  var hasLowerCasedCharacters = confirm(
+    'Click OK to confirm including lowercase characters.'
+  );
+
+// Variable to store boolean regarding the inclusion of uppercase characters
+   var hasUpperCasedCharacters = confirm(
+    'Click OK to confirm including uppercase characters.'
+  );
+
+// Conditional statement to check if user does not include any types of characters. 
+  if (
+    hasSpecialCharacters === false &&
+    hasNumericCharacters === false &&
+    hasLowerCasedCharacters === false &&
+    hasUpperCasedCharacters === false
+  ) {
+    alert('Must select at least one character type');
+    return null;
+  }
+
+// Object to store user input
+  var passwordOptions = {
+    length: length,
+    hasSpecialCharacters: hasSpecialCharacters,
+    hasNumericCharacters: hasNumericCharacters,
+    hasLowerCasedCharacters: hasLowerCasedCharacters,
+    hasUpperCasedCharacters: hasUpperCasedCharacters,
+  };
+
+  return passwordOptions;
+}
+
+// Function for getting a random element from an array
+function getRandom(arr) {
+  var randIndex = Math.floor(Math.random() * arr.length);
+  var randElement = arr[randIndex];
+
+  return randElement;
+}
+
+// Function to generate password with user input
+function generatePassword() {
+  var options = getPasswordOptions();
+// Variable to store password as it's being concatenated
+  var result = [];
+
+// Array to store types of characters to include in password
+  var possibleCharacters = [];
+
+// Array to contain one of each type of chosen character to ensure each will be used
+  var guaranteedCharacters = [];
+
+// Check if an options object exists, if not exit the function
+  if (!options) return null;
+
+// Push new random special character to guaranteedCharacters
+  if (options.hasSpecialCharacters) {
+    possibleCharacters = possibleCharacters.concat(specialCharacters);
+    guaranteedCharacters.push(getRandom(specialCharacters));
+  }
+
+
