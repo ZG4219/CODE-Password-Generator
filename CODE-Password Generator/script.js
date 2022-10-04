4f22bb6e92b750518bd57f398c4c775bd5635642
Dionne Angeles <jr8dionne@yahoo.com>
2022-10-04 14:18:57 -0700

    added array of special characters to be included in password

--- CODE-Password Generator/script.js
+++ CODE-Password Generator/script.js
@@ -1,14 +1,27 @@
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
+// Array of special characters to be included in password
+var specialCharacters = [
+  '@',
+  '%',
+  '+',
+  '\\',
+  '/',
+  "'",
+  '!',
+  '#',
+  '$',
+  '^',
+  '?',
+  ':',
+  ',',
+  ')',
+  '(',
+  '}',
+  '{',
+  ']',
+  '[',
+  '~',
+  '-',
+  '_',
+  '.',
+];
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
-}
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
